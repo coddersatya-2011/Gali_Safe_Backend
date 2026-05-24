4d83a6ed316ccd56b7cefab488ca8da836fc8a04
satyam singh khatri <work.satyam20@gmail.com>
2026-05-25 03:00:40 +0530

    chore: GALISAFE APP PROJECT STARTED - BACKEND-initial project setup and springboot skeleton ready. chore: BACKEND- connect MySQL database and add dependencies and configure Hibernate DDL auto update feat: BACKEND- add User entity model with Lombok annotations feat: BACKEND- add RiskZone entity for tracking unsafe and geolocation area feat: BACKEND- add SosAlert entity model mapping users to emergency triggers feat: BACKEND- add EmergencyContect entity for safety circle feat: BACKEND- add UserRepository interface with custom findByEmail method feat: BACKEND- add RiskZoneRepository interface for spatial data operations feat: BACKEND- add SosAlertRepository interface with user-specific query methods feat: BACKEND- add EmergencyContectRepository interface to manage user contacts feat: BACKEND- add main application class to bootstrap GalisafeBackend test: BACKEND- add context loadds test for application startup verification

---
+++ galisafe-backend/src/test/java/com/galisafe/galisafe_backend/GalisafeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.galisafe.galisafe_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GalisafeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
