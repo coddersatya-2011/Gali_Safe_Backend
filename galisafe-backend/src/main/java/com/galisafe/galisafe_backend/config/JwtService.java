92ed315dab14336a1b6bf689e91c953fbf3da033
satyam singh khatri <work.satyam20@gmail.com>
2026-05-25 03:04:41 +0530

    chore: GALISAFE APP PROJECT STARTED - BACKEND-initial project setup and springboot skeleton ready. chore: BACKEND- connect MySQL database and add dependencies and configure Hibernate DDL auto update feat: BACKEND- add User entity model with Lombok annotations feat: BACKEND- add RiskZone entity for tracking unsafe and geolocation area feat: BACKEND- add SosAlert entity model mapping users to emergency triggers feat: BACKEND- add EmergencyContect entity for safety circle feat: BACKEND- add UserRepository interface with custom findByEmail method feat: BACKEND- add RiskZoneRepository interface for spatial data operations feat: BACKEND- add SosAlertRepository interface with user-specific query methods feat: BACKEND- add EmergencyContectRepository interface to manage user contacts feat: BACKEND- add main application class to bootstrap GalisafeBackend test: BACKEND- add context loadds test for application startup verification feat: BACKEND- implement JwtService for token generation and validation

---
+++ galisafe-backend/src/main/java/com/galisafe/galisafe_backend/config/JwtService.java
@@ -0,0 +1,46 @@
+package com.galisafe.galisafe_backend.config;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
+
+@Service
+public class JwtService {
+    private static final String SECRET = "galisafe-secret-key-must-be-32-chars!!";
+    private static final long EXPIRY = 1000 * 60 * 60 * 24; // 24 hours
+
+    private Key getKey() {
+        return Keys.hmacShaKeyFor(SECRET.getBytes());
+    }
+
+    public String generateToken(String email) {
+        return Jwts.builder()
+                .setSubject(email)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRY))
+                .signWith(getKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public String extractEmail(String token) {
+        return getClaims(token).getSubject();
+    }
+
+    public boolean isTokenValid(String token) {
+        return getClaims(token).getExpiration().after(new Date());
+    }
+
+    private Claims getClaims(String token) {
+        return Jwts.parser()
+                .setSigningKey(getKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+ }
+
