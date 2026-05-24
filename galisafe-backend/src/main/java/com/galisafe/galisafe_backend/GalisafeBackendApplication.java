1d9343c91a32378ebf60523b28e2d7472a62aa4f
satyam singh khatri <work.satyam20@gmail.com>
2026-05-25 02:58:31 +0530

    chore: GALISAFE APP PROJECT STARTED - BACKEND-initial project setup and springboot skeleton ready. chore: BACKEND- connect MySQL database and add dependencies and configure Hibernate DDL auto update feat: BACKEND- add User entity model with Lombok annotations feat: BACKEND- add RiskZone entity for tracking unsafe and geolocation area feat: BACKEND- add SosAlert entity model mapping users to emergency triggers feat: BACKEND- add EmergencyContect entity for safety circle feat: BACKEND- add UserRepository interface with custom findByEmail method feat: BACKEND- add RiskZoneRepository interface for spatial data operations feat: BACKEND- add SosAlertRepository interface with user-specific query methods feat: BACKEND- add EmergencyContectRepository interface to manage user contacts feat: BACKEND- add main application class to bootstrap GalisafeBackend

---
+++ galisafe-backend/src/main/java/com/galisafe/galisafe_backend/GalisafeBackendApplication.java
@@ -0,0 +1,11 @@
+package com.galisafe.galisafe_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GalisafeBackendApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(GalisafeBackendApplication.class, args);
+	}
+}
